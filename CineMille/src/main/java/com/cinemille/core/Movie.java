5dca4787b619526392b1d83b9f64a2509b3d91ed
CodeBrothersYT <ytcodebrothers@gmail.com>
2023-07-03 14:53:23 +0200

    :DEV: create project and first empty Movie entity

---
+++ CineMille/src/main/java/com/cinemille/core/Movie.java
@@ -0,0 +1,18 @@
+package com.cinemille.core;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
