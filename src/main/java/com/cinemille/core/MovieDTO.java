d46ab3b70747f26ba24ad4320989e169f4e11c21
CodeBrothersYT <ytcodebrothers@gmail.com>
2023-07-05 11:38:54 +0200

    :DEV: move .git and create CineMilleFE

--- src/main/java/com/cinemille/core/MovieDTO.java
+++
@@ -1,3 +0,0 @@
-package com.cinemille.core;
-
-public record MovieDTO(String title, String description, long length) {}
--- src/main/java/com/cinemille/web/MovieFacade.java
+++
@@ -1,4 +0,0 @@
-package com.cinemille.web;
-
-public interface MovieFacade {
-}
